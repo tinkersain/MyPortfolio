ee47bfee5d163a255e47871b4fdae9f09526ef90
addymistrel <149488588+tinkersain@users.noreply.github.com>
2024-04-10 18:26:00 +0530

    every thing completed just a little bit left

---
+++ src/components/Resume.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <div>
+      <iframe title="PDF Viewer" src="/Tanisha_Resume.pdf" />
+    </div>
+  );
+};
+
+export default Resume;
